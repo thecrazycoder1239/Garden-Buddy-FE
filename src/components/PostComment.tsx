cae48290466dfad9cdabb68816bd66fe0a14e97b
Ede Harrison <113683532+edeharrison@users.noreply.github.com>
2023-03-31 08:58:46 +0100

    values in react-app-env.d.ts for single plant page

--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -1,34 +1,38 @@
 // Hooks
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function PostComment() {
-    const [commentInProgress, setCommentInProgress] = useState("")
-    const [newComment, setNewComment] = useState()
+  const [commentInProgress, setCommentInProgress] = useState("");
+  const [newComment, setNewComment] = useState("");
 
-    const handleChange = (
-      e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-      setCommentInProgress(e.currentTarget.value);
-    };
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setCommentInProgress(e.currentTarget.value);
+  };
 
-    console.log(newComment)
-    // const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     e.preventDefault()
-    // }
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    setNewComment(commentInProgress);
+    setCommentInProgress("")
+};
 
-    return (
-      <form className="post-comment">
-        <label htmlFor="post-comment-textarea">Post a comment</label>
-        <input 
-            id="post-comment-textarea" 
-            type="textarea"
-            value={commentInProgress}
-            onChange={handleChange}
-        ></input>
-        <button 
-            type="submit"
-            // onClick={handleSubmit}
-        >Post</button>
-      </form>
-    );
+  return (
+    <form className="post-comment" onSubmit={handleSubmit}>
+      <label htmlFor="post-comment-textarea">Post a comment</label>
+      <input
+        id="post-comment-textarea"
+        type="textarea"
+        value={commentInProgress}
+        onChange={handleChange}
+      ></input>
+      <button 
+        type="submit" 
+        >
+        Post
+      </button>
+    </form>
+  );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,9 +1,4 @@
 /// <reference types="react-scripts" />
-type Attributes = {
-    description: string,
-    main_image_path: string,
-    sun_requirements: string
-}
 
 interface GrowStuffCrop {
   //from '/crops'
@@ -15,12 +10,20 @@ interface GrowStuffCrop {
   description: string;
   thumbnail_url: string;
 
-  // from '/crops/_id'
+  // from '/crops/:id'
+  median_lifespan: number;
+  median_days_to_first_harvest: number;
+  median_days_to_last_harvest: number;
+  perennial: boolean;
+
   openfarm_data: {
     attributes: {
       description: string;
       main_image_path: string;
       sun_requirements: string;
+      sowing_method: string;
+      height: number;
+      spread: number;
     };
   };
 }
