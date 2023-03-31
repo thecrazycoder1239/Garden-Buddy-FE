8e423629d16713d7eeb9c3f42047acb9d63838da
Mairi Wood <MairiMW@Eraon-Mac.local>
2023-03-31 13:46:15 +0100

    add-plant-button with api progress

--- src/components/PlantCards.tsx
+++ src/components/PlantCards.tsx
@@ -1,32 +1,14 @@
-import CalendarPicker from "./CalendarPicker";
+import SinglePlantCard from "./SinglePlantCard";
 
 // Hooks
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../contexts/User";
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // API
-import { getPlants, postPlantToUser } from "../utils/api";
-
-// Icons
-import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { getPlants } from "../utils/api";
 
 export default function PlantCards() {
   const [isLoadingPlants, setIsLoadingPlants] = useState(false);
   const [plants, setPlants] = useState([]);
-  const [date, setDate] = useState(new Date());
-
-  const {user} = useContext(UserContext);
-  
-  const handleClick = (e: any) => {
-    e.preventDefault();
-    if (user) {
-      postPlantToUser(user, e.target.value, date)
-      .then(console.log);
-    }
-    
-    return({msg: 'added'})
-  }
 
   useEffect(() => {
     setIsLoadingPlants(true);
@@ -49,32 +31,7 @@ export default function PlantCards() {
       <ul className="plant-cards">
         {plants.map((plant) => {
           return (
-            <div className='single-plant-container'>
-              <CalendarPicker date={date} setDate={setDate}/>
-              <Link to={`/all-plants/${plant["_id"]}`}>
-              <li className="plant-card" key={plant["_id"]}>
-                {user? 
-              <button className="add-plant-button" onClick={handleClick} value={plant["_id"]}>Plant!</button> 
-                : <p className="add-plant-button-no-user-message">Sign in to add plant</p>}
-              {/* <li onClick={() => setSinglePlant(plant["_id"])} className="plant-card" key={plant["_id"]}> */}
-                <div>
-                  <h2>{plant["name"]}</h2>
-                  <p>{plant["scientific_name"]}</p>
-                  <div className="description-container">
-                    <p className="plant-description">{plant["description"]}</p>
-                  </div>
-                  <div className="votes">
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiOutlineStar />
-                    <AiOutlineStar />
-                  </div>
-                </div>
-                <img alt='plant' src={plant["thumbnail_url"]} />
-            </li>
-            </Link>
-            </div>
+              <SinglePlantCard plant={plant} key={plant["_id"]}/>
           );
         })}
       </ul>
