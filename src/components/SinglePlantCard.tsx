704d6c0823afb3a095aa6c8c1f0180b20e3c8ed1
Ede Harrison <113683532+edeharrison@users.noreply.github.com>
2023-04-04 09:15:56 +0100

    greyscale singleplantcard styling

--- src/components/SinglePlantCard.tsx
+++ src/components/SinglePlantCard.tsx
@@ -11,82 +11,86 @@ import { postPlantToUser } from "../utils/api";
 // Icons
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
-export default function SinglePlantCard({plant} : {plant: GrowStuffCrop}) {
+export default function SinglePlantCard({ plant }: { plant: GrowStuffCrop }) {
+  const [date, setDate] = useState(new Date());
 
-    const [date, setDate] = useState(new Date());
+  const [buttonActive, setButtonActive] = useState(true);
+  const [buttonStyle, setButtonStyle] = useState({
+    transition: "0s",
+    background: "#333",
+  });
 
-    const [buttonActive, setButtonActive] = useState(true);
-    const [buttonStyle, setButtonStyle] = useState({transition: '0s', background: '#f1f1f1'});
+  const { user } = useContext(UserContext);
 
-	const { user } = useContext(UserContext);
+  function changeColor() {
+    setButtonStyle({
+      transition: "0.3s",
+      background: "#1ec765",
+    });
+  }
 
+  function fadeColor() {
+    setButtonStyle({
+      transition: "0.3s",
+      background: "#333",
+    });
+  }
 
-    function changeColor(){
-            setButtonStyle({
-                transition: '0.3s',
-                background: '#1ec765'
-            })
-        }
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    if (user) {
+      setButtonActive(false);
+      changeColor();
+      postPlantToUser(user, e.target.value, date)
+        .then(console.log)
+        .finally(() => {
+          let timer = setTimeout(() => {
+            fadeColor();
+            setButtonActive(true);
+          }, 3000);
+        });
+    }
+    return { msg: "added" };
+  };
 
-    function fadeColor(){
-        setButtonStyle({
-            transition: '0.3s',
-            background: '#f1f1f1'
-            })
-        }
-
-	const handleClick = (e: any) => {
-		e.preventDefault();
-		if (user) {
-            setButtonActive(false)
-            changeColor();
-			postPlantToUser(user, e.target.value, date).then(console.log)
-            .finally(() => {
-                    let timer = setTimeout(() => {
-                        fadeColor();
-                        setButtonActive(true)
-                }, 3000)
-            })
-		}
-		return { msg: "added" };
-	};
-
-	return (
-		<li className="single-plant-container">
-			<CalendarPicker date={date} setDate={setDate} />
-			<Link to={`/all-plants/${plant["_id"]}`}>
-				<div className="plant-card" key={plant["_id"]}>
-					{user ? (
-						<button
-                            style={buttonStyle}
-							className="add-plant-button"
-							onClick={handleClick}
-							value={plant["_id"]}
-                            disabled={!buttonActive}
-						>
-							Plant!
-						</button>
-					) : (
-						<p className="add-plant-button-no-user-message">Sign in to add plant</p>
-					)}
-					{/* <li onClick={() => setSinglePlant(plant["_id"])} className="plant-card" key={plant["_id"]}> */}
-					<div>
-						<h2>{plant["name"]}</h2>
-						<p>{plant["scientific_name"]}</p>
-						<div className="description-container">
-							<p className="plant-description">{plant["description"]}</p>
-						</div>
-						<div className="votes">
-							<AiFillStar />
-							<AiFillStar />
-							<AiFillStar />
-							<AiOutlineStar />
-							<AiOutlineStar />
-						</div>
-					</div>
-					<img alt="plant" src={plant["thumbnail_url"]} />
-				</div>
-			</Link>
-		</li>
-	);
+  return (
+    <li className="single-plant-container" key={plant["_id"]}>
+      <Link to={`/all-plants/${plant["_id"]}`}>
+        {/* <li onClick={() => setSinglePlant(plant["_id"])} className="plant-card" key={plant["_id"]}> */}
+        <div>
+          <img alt="plant" src={plant["thumbnail_url"]} />
+          <div className="description-container">
+            <h2>{plant["name"]}</h2>
+            <p><i>{plant["scientific_name"]}</i></p>
+            <p className="plant-description">{plant["description"]}</p>
+            <div className="votes">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+              <AiOutlineStar />
+            </div>
+          </div>
+        </div>
+      </Link>
+      {user ? (
+        <div className="set-plant-date">
+          <CalendarPicker date={date} setDate={setDate} />
+          <button
+            style={buttonStyle}
+            className="form"
+            onClick={handleClick}
+            value={plant["_id"]}
+            disabled={!buttonActive}
+          >
+            Plant!
+          </button>
+        </div>
+      ) : (
+        <Link to="/log-in" className="add-plant-button-no-user-message">
+          Sign in to add plant
+        </Link>
+      )}
+    </li>
+  );
 }
